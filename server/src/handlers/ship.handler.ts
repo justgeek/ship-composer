814770055a7e27d01a0238be73b9c2d05e2950b6
TaherM <mohamed.taher@cartrust.com>
2021-08-30 17:05:13 +0200

    [Server] add ship entity handler

--- server/src/handlers/ship.handler.ts
+++ server/src/handlers/ship.handler.ts
@@ -1,14 +1,27 @@
+import { Request, Response } from "express";
+import { shipController } from "../controllers/ship.controller";
+
 export default class ShipHandler {
   //Do extra stuff if necessary Here
   //....
   // Redirect to controller
-  handleGetAllShips(req: Request, res: Response) {}
+  handleGetAllShips(req: Request, res: Response) {
+    shipController.getAllShip(req, res);
+  }
 
-  handleGetShipById(req: Request, res: Response) {}
+  handleGetShipById(req: Request, res: Response) {
+    shipController.getShipByID(req, res);
+  }
 
-  handleCreateShip(req: Request, res: Response) {}
+  handleCreateShip(req: Request, res: Response) {
+    shipController.createShip(req, res);
+  }
 
-  handleUpdateShip(req: Request, res: Response) {}
+  handleUpdateShip(req: Request, res: Response) {
+    shipController.updateShip(req, res);
+  }
 
-  handleDeleteShip(req: Request, res: Response) {}
+  handleDeleteShip(req: Request, res: Response) {
+    shipController.deleteShip(req, res);
+  }
 }
