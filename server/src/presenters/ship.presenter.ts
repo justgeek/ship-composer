581b0ade7921ceae8008e85afbf6926ebb49426d
TaherM <mohamed.taher@cartrust.com>
2021-08-30 17:05:28 +0200

    [Server] add ship entity presenter

--- server/src/presenters/ship.presenter.ts
+++ server/src/presenters/ship.presenter.ts
@@ -1,3 +1,22 @@
-class ShipPresenter {}
+import { Response } from "express";
+import Ship from "../models/Ship";
+
+class ShipPresenter {
+  presentShip(res: Response, ship: Ship) {
+    res.json({
+      ...ship,
+    });
+  }
+
+  presentShips(res: Response, ships: Ship[]) {
+    res.json(ships);
+  }
+
+  presentShipId(res: Response, id: string) {
+    res.json({
+      id,
+    });
+  }
+}
 
 export const shipPresenter = new ShipPresenter();
