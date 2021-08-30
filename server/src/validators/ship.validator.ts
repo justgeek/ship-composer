cfc3dee9cb042517d2e7446929a71b6165699d1e
TaherM <mohamed.taher@cartrust.com>
2021-08-30 17:03:29 +0200

    [Server] add basic validation rules

--- server/src/validators/ship.validator.ts
+++ server/src/validators/ship.validator.ts
@@ -1,1 +1,33 @@
-export default class ShipValidator {}
+import { body, param } from "express-validator";
+
+export default class ShipValidator {
+  private codePattern = /[A-Z]{4}-[0-9]{4}-[A-Z]{1}[0-9]{1}/;
+  private uuidPattern =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  private writeRules = [
+    body("name", "name is required").exists(),
+    body("name", "name is invalid").isAlpha("en-US", { ignore: " " }),
+    body("width", "width is required").exists(),
+    body("width", "width must be  int").isInt(),
+    body("length", "length is required").exists(),
+    body("length", "length must be  int").isInt(),
+    body("code", "code is required").exists(),
+    body(
+      "code",
+      `wrong code format, correct pattern is ${this.codePattern}`
+    ).matches(this.codePattern, "g"),
+  ];
+
+  private readRules = [
+    param("id", `wrong id format, must be uuid`).matches(this.uuidPattern),
+  ];
+  validateShipId() {
+    return this.readRules;
+  }
+  validateCreateShipPayload() {
+    return this.writeRules;
+  }
+  validateUpdateShipPayload() {
+    return this.writeRules;
+  }
+}
